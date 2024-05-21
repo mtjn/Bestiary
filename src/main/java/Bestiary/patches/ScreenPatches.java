45483aa831a4f8e2715e6a5714316991185d330b
Neoshrimp <shadow638@protonmail.com>
2024-05-21 23:34:20 +0300

    Unhover monsters on bestiary overlay, selectively patch AbtractRoom.update

--- src/main/java/Bestiary/patches/ScreenPatches.java
+++ src/main/java/Bestiary/patches/ScreenPatches.java
@@ -3,53 +3,70 @@ package Bestiary.patches;
 import Bestiary.BestiaryMod;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
+import com.megacrit.cardcrawl.core.OverlayMenu;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.TipHelper;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.scenes.AbstractScene;
+import com.megacrit.cardcrawl.screens.DrawPileViewScreen;
+import com.megacrit.cardcrawl.screens.DungeonMapScreen;
+import com.megacrit.cardcrawl.ui.FtueTip;
+import com.megacrit.cardcrawl.ui.buttons.DynamicBanner;
 import com.megacrit.cardcrawl.ui.panels.TopPanel;
 
 // Special thanks to blanktheevil (infinitespire)
 
 public class ScreenPatches {
-    @SpirePatch(
-            clz = TopPanel.class,
-            method = "update"
-    )
-    public static class TopPanelSuppressInputPatch {
-        @SpirePrefixPatch
-        public static SpireReturn<Void> Prefix(TopPanel __instance) {
-            if (BestiaryMod.showOverlay)
-                return SpireReturn.Return(null);
-            else
-                return SpireReturn.Continue();
-        }
-    }
 
-    @SpirePatch(
-            clz = TipHelper.class,
-            method = "render"
-    )
-    public static class TipSuppressorPatch {
-        @SpirePrefixPatch
-        public static SpireReturn<Void> Prefix(SpriteBatch sb) {
-            if (BestiaryMod.showOverlay)
-                return SpireReturn.Return(null);
-            else
-                return SpireReturn.Continue();
-        }
-    }
 
     @SpirePatch(
             clz = AbstractDungeon.class,
             method = "update"
     )
-    public static class AbstractDungeonUpdateSuppressPatch {
+    public static class AbstractDungeonUpdatePatch {
         @SpirePrefixPatch
-        public static SpireReturn<Void> Prefix(AbstractDungeon __instance) {
+        public static void Prefix() {
             BestiaryMod.update();
+        }
+    }
 
-            if (BestiaryMod.showOverlay)
+
+    @SpirePatch(
+        clz = AbstractRoom.class,
+        method = "update"
+    )
+    @SpirePatch(
+        clz = AbstractRoom.class,
+        method = "eventControllerInput"
+    )
+    @SpirePatch(
+        clz = AbstractScene.class,
+        method = "update"
+    )
+    @SpirePatch(
+        clz = TopPanel.class,
+        method = "update"
+    )
+    @SpirePatch(
+        clz = FtueTip.class,
+        method = "update"
+    )
+    @SpirePatch(
+        clz = DungeonMapScreen.class,
+        method = "update"
+    )
+    @SpirePatch(
+        clz = OverlayMenu.class,
+        method = "update"
+    )
+    public static class UpdateSuppressPatch {
+        @SpirePrefixPatch
+        public static SpireReturn<Void> Prefix() {
+            if (BestiaryMod.showOverlay) 
                 return SpireReturn.Return(null);
             else
                 return SpireReturn.Continue();
